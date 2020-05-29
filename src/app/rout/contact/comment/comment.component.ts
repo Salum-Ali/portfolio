aa7a9ee7925cb398513d5d728f626bbf2c9656ce
Salum Muhammed <salumalimuhammed@yahoo.com>
2020-05-29 14:23:33 +0100

    FEATURE-START: TemplateForms StageOne

--- src/app/rout/contact/comment/comment.component.ts
+++ src/app/rout/contact/comment/comment.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.sass']
 })
 export class CommentComponent implements OnInit {
+  name: string;
+  email: string;
+  message: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  processForm() {
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
+    alert(allInfo);
   }
 
 }
